1c1b53fa65e7f9548ee49e9f478225bdef4d04e9
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-10-30 21:09:19 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,13 @@ const heroSlider = new Swiper('#hero-slider', {
   autoplay: {
     delay: 4500,
   },
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
 
