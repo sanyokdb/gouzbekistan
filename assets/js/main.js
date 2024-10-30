0743acba5e5af291b254c8b75b8ce60f85c51802
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-10-30 16:43:42 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+const heroSlider = new Swiper('#hero-slider', {
+  loop: true,
+  effect: "fade",
+  // autoplay: {
+  //   delay: 4500,
+  // },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
 // scrollUp
 $(window).scroll(function () {
   if ($(this).scrollTop() > 50) {
