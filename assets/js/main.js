3273a7fba672b3bee45ae9eb8e1ddb855de75ba5
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-11-09 18:02:39 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
-const heroSlider = new Swiper('#hero-slider', {
+const heroSlider = new Swiper("#hero-slider", {
   loop: true,
   effect: "fade",
   autoplay: {
     delay: 4500,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
-
 
 // scrollUp
 $(window).scroll(function () {
@@ -31,4 +30,26 @@ $("#scrollUp").click(function () {
     500
   );
   return false;
+});
+
+// review-slider
+const review = new Swiper(".review-slider", {
+  speed: 600,
+  autoHeight: true,
+  // loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+    },
+  },
 });
